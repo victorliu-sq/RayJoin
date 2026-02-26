e2bf4003b2be7d8774ad4febb1d127f4661cab1e
victor-liu <jiaxinliu.victor@gmail.com>
2026-02-25 19:27:41 -0500

    update bounding_box

--- src/vk/map/bounding_box.h
+++ src/vk/map/bounding_box.h
@@ -6,20 +6,16 @@
 namespace rayjoin {
 namespace vk {
 template <typename COORD_T>
-struct BoundingBox{
+struct BoundingBox {
   COORD_T min_x, min_y, max_x, max_y;
-};
 
-template <typename COORD_T>
-inline BoundingBox<COORD_T> bboxInitHost() {
-  BoundingBox<COORD_T> b{};
-  b.min_x = std::numeric_limits<COORD_T>::max();
-  b.min_y = std::numeric_limits<COORD_T>::max();
-  b.max_x = std::numeric_limits<COORD_T>::lowest();
-  b.max_y = std::numeric_limits<COORD_T>::lowest();
-  return b;
-}
-}
+  BoundingBox()
+      : min_x(std::numeric_limits<COORD_T>::max()),
+        min_y(std::numeric_limits<COORD_T>::max()),
+        max_x(std::numeric_limits<COORD_T>::lowest()),
+        max_y(std::numeric_limits<COORD_T>::lowest()) {}
+};
+}  // namespace vk
 
 }  // namespace rayjoin
 #endif  // RAYJOIN_MAP_BOUNDING_BOX_H
--- src/vk/map/context.h
+++ src/vk/map/context.h
@@ -1,10 +1,76 @@
 #ifndef RAYJOIN_CONTEXT_H
 #define RAYJOIN_CONTEXT_H
 
+#include "vk/map/bounding_box.h"
+
 namespace rayjoin {
 namespace vk {
 
-} // namespace vk
-} // namespace rajoin
+template <typename COORD_T, typename COEFFICIENT_T = __int128>
+class Context {
+ public:
+  using coord_t = COORD_T;
+  using coefficient_t = COEFFICIENT_T;
+  // using scaling_t = Scaling<coord_t>;
+  // using internal_coord_t = typename scaling_t::internal_coord_t;
+  using planar_graph_t = PlanarGraph<coord_t>;
+  // using map_t = Map<internal_coord_t, coefficient_t>;
+  using bounding_box_t = BoundingBox<coord_t>;
+
+  Context() = delete;
+
+  explicit Context(
+      const std::array<std::shared_ptr<planar_graph_t>, 2>& planar_graphs)
+      : planar_graphs_(planar_graphs) {
+    for (auto pgraph : planar_graphs) {
+      if (pgraph != nullptr) {
+        auto& bb = pgraph->bb;
+        bb_.min_x = std::min(bb_.min_x, bb.min_x);
+        bb_.max_x = std::max(bb_.max_x, bb.max_x);
+        bb_.min_y = std::min(bb_.min_y, bb.min_y);
+        bb_.max_y = std::max(bb_.max_y, bb.max_y);
+      }
+    }
+
+    // scaling_ = Scaling<coord_t>(bb_);
+#ifndef NDEBUG
+    // scaling_.DebugPrint();
+#endif
+    LOG(INFO) << "Bounding Box, Bottom-left: (" << bb_.min_x << ", "
+              << bb_.min_y << "), Top-right: (" << bb_.max_x << ", "
+              << bb_.max_y << ")";
+    // auto internal_min_x = scaling_.ScaleX(bb_.min_x);
+    // auto internal_min_y = scaling_.ScaleY(bb_.min_y);
+    // auto internal_max_x = scaling_.ScaleX(bb_.max_x);
+    // auto internal_max_y = scaling_.ScaleY(bb_.max_y);
+    // LOG(INFO) << "Scaled Bounding Box, Bottom-left: (" << internal_min_x <<
+    // ", "
+    //           << internal_min_y << "), Top-right: (" << internal_max_x << ",
+    //           "
+    //           << internal_max_y << ")";
+    // LOG(INFO) << "Unscaled Bounding Box, Bottom-left: ("
+    //           << scaling_.UnscaleX(internal_min_x) << ", "
+    //           << scaling_.UnscaleY(internal_min_y) << "), Top-right: ("
+    //           << scaling_.UnscaleX(internal_max_x) << ", "
+    //           << scaling_.UnscaleY(internal_max_y) << ")";
+
+    // char result[PATH_MAX];
+    // ssize_t count = readlink("/proc/self/exe", result, PATH_MAX);
+    // const char* path;
+    // if (count != -1) {
+    //   path = dirname(result);
+    // }
+    // exec_root = std::string(path);
+  }
+
+ private:
+  std::array<std::shared_ptr<planar_graph_t>, 2> planar_graphs_;
+  // std::array<std::shared_ptr<map_t>, 2> maps_;
+  bounding_box_t bb_;
+  // scaling_t scaling_;
+  // std::string exec_root;  // folder of binary
+};
+}  // namespace vk
+}  // namespace rayjoin
 
 #endif  // RAYJOIN_CONTEXT_H
