44c72b72eb817d42fadde114b57d98b9c53e717f
victor-liu <jiaxinliu.victor@gmail.com>
2026-03-11 22:17:53 -0400

    update rayjoin using ag=0

--- src/flag/flags.cc
+++ src/flag/flags.cc
@@ -12,13 +12,15 @@ DEFINE_bool(fau, false, "Free after use");
 DEFINE_int32(warmup, 5, "");
 DEFINE_int32(repeat, 5, "");
 DEFINE_string(serialize, "", "");
-DEFINE_int32(ag, 1, "AG method, 0: disable, 1: paper stated, 2: simple");
+// DEFINE_int32(ag, 1, "AG method, 0: disable, 1: paper stated, 2: simple");
+DEFINE_int32(ag, 0, "AG method, 0: disable, 1: paper stated, 2: simple");
 DEFINE_int32(ag_iter, 5, "Max iteration of grouping");
 DEFINE_int32(win, 32, "Window size for AABB compression");
 DEFINE_double(enlarge, 5, "Threshold for AABB merge");
 // Queries
 DEFINE_int32(sample_map_id, -1, "0->poly1 / 1->poly2");
-DEFINE_string(sample, "",
+DEFINE_string(sample,
+              "",
               "Whether to sample edges or map. Sampling map keeps every chain, "
               "so it holds the topology of original map. As a result, it looks "
               "like a down scaled map");
@@ -28,6 +30,4 @@ DEFINE_int32(seed, 0, "Random seed for sampling and data generating");
 DEFINE_double(gen_t, 0.1, "Len of generated line segments");
 DEFINE_int32(gen_n, 10000, "Number of generated edges/points");
 DEFINE_bool(histo, false, "Whether to print histogram");
-DEFINE_bool(
-    profile, false,
-    "Enable profiling, some counters are only compiled available under debug");
+DEFINE_bool(profile, false, "Enable profiling, some counters are only compiled available under debug");
