3f108c665754da474dcbd4514bbad3ecf692c4b7
victor-liu <jiaxinliu.victor@gmail.com>
2026-03-30 21:21:43 -0400

    add test scripts for vk-scaling

--- scripts/test/run_test_1_dump.sh
+++ scripts/test/run_test_1_dump.sh
@@ -11,7 +11,7 @@ optix_output="${base_dir}/results/br_countyXbr_soil_result_optix.txt"
 
 # =========================================================
 # Run Vulkan with dumps enabled
-./build/bin/polyover_vk_exec_ns \
+./build/bin/polyover_vk_exec \
   -poly1 "$poly1" \
   -poly2 "$poly2" \
   -output "$vk_output" \
@@ -51,40 +51,35 @@ source "${script_dir}/compare_files.sh"
 # -------------------------
 # PIP comparisons
 # -------------------------
-run_compare "PIP map 0" \
-  "${pip_dir}/optix_pip_map_0.csv" \
-  "${pip_dir}/vulkan_pip_map_0.csv" \
-  "$pip_diff_dir" || status=1
-
-run_compare "PIP map 1" \
-  "${pip_dir}/optix_pip_map_1.csv" \
-  "${pip_dir}/vulkan_pip_map_1.csv" \
-  "$pip_diff_dir" || status=1
+#run_compare "PIP map 0" \
+#  "${pip_dir}/optix_pip_map_0.csv" \
+#  "${pip_dir}/vulkan_pip_map_0.csv" \
+#  "$pip_diff_dir" || status=1
+#
+#run_compare "PIP map 1" \
+#  "${pip_dir}/optix_pip_map_1.csv" \
+#  "${pip_dir}/vulkan_pip_map_1.csv" \
+#  "$pip_diff_dir" || status=1
 
 # -------------------------
 # LSI comparisons
 # -------------------------
-run_compare "LSI map 0" \
-  "${lsi_dir}/optix_lsi_map_0.csv" \
-  "${lsi_dir}/vulkan_lsi_map_0.csv" \
-  "$lsi_diff_dir" || status=1
-
-#run_compare "LSI map 1" \
-#  "${lsi_dir}/optix_lsi_map_1.csv" \
-#  "${lsi_dir}/vulkan_lsi_map_1.csv" \
+#run_compare "LSI map 0" \
+#  "${lsi_dir}/optix_lsi_map_0.csv" \
+#  "${lsi_dir}/vulkan_lsi_map_0.csv" \
 #  "$lsi_diff_dir" || status=1
 
 # -------------------------
 # Midpoint PIP comparisons
 # -------------------------
-run_compare "PIP midpoint map 0" \
-  "${mid_dir}/optix_pipmid_map_0.csv" \
-  "${mid_dir}/vulkan_pipmid_map_0.csv" \
-  "$mid_diff_dir" || status=1
-
-run_compare "PIP midpoint map 1" \
-  "${mid_dir}/optix_pipmid_map_1.csv" \
-  "${mid_dir}/vulkan_pipmid_map_1.csv" \
-  "$mid_diff_dir" || status=1
+#run_compare "PIP midpoint map 0" \
+#  "${mid_dir}/optix_pipmid_map_0.csv" \
+#  "${mid_dir}/vulkan_pipmid_map_0.csv" \
+#  "$mid_diff_dir" || status=1
+#
+#run_compare "PIP midpoint map 1" \
+#  "${mid_dir}/optix_pipmid_map_1.csv" \
+#  "${mid_dir}/vulkan_pipmid_map_1.csv" \
+#  "$mid_diff_dir" || status=1
 
 exit "$status"
