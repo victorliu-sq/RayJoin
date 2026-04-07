3ef68c1d6fb1492f6b9ee38d3593bc59a11fceb1
victor-liu <jiaxinliu.victor@gmail.com>
2026-04-06 22:54:55 -0400

    update RAII wrapper & stderr printer

--- src/app/04_overlay_vk_ns/main.cc
+++ src/app/04_overlay_vk_ns/main.cc
@@ -7,33 +7,9 @@
 #include "vk/core/vk_global_context.h"
 
 int main(int argc, char* argv[]) {
-  FLAGS_stderrthreshold = 0;
-  auto flags = CreateGflagsGuard(argc, argv, "Usage: -poly1 -poly2");
-  auto g_glog_guard = CreateGlogGuard("polyover_vk");
-  auto vk_runtime = CreateVkGlobalRuntime();
+  auto gflag_guard = CreateGflagsGuardWithStderrInfo(argc, argv, "Usage: -poly1 -poly2");
+  auto glog_guard = CreateGlogGuard("polyover_vk");
 
-  rayjoin::OverlayConfig config;
-  std::string exec_path = argv[0];
-
-  config.map1_path = FLAGS_poly1;
-  config.map2_path = FLAGS_poly2;
-  config.output_path = FLAGS_output;
-  config.serialize_prefix = FLAGS_serialize;
-  config.grid_size = FLAGS_grid_size;
-  config.xsect_factor = FLAGS_xsect_factor;
-  config.mode = FLAGS_mode;
-  config.exec_root = exec_path.substr(0, exec_path.find_last_of("/"));
-  config.check = FLAGS_check;
-  config.fau = FLAGS_fau;
-  config.profile = FLAGS_profile;
-  config.win = FLAGS_win;
-  config.ag = FLAGS_ag;
-  config.ag_iter = FLAGS_ag_iter;
-  config.enlarge = FLAGS_enlarge;
-
-  // dump
-  config.dump_results = FLAGS_dump_results;
-  config.dump_dir = FLAGS_dump_dir;
-
+  auto config = rayjoin::CreateOverlayConfig(argv[0]);
   rayjoin::vk::RunOverlayNS(config);
 }
--- src/app/05_overlay_native/main.cc
+++ src/app/05_overlay_native/main.cc
@@ -3,45 +3,13 @@
 // #include "core/run_overlay.h"
 #include "core/run_overlay_native.h"
 #include "flag/flags.h"
+#include "util/guard_gflag.h"
+#include "util/guard_glog.h"
 
 int main(int argc, char* argv[]) {
-  FLAGS_stderrthreshold = 0;
+  auto flags = CreateGflagsGuardWithStderrInfo(argc, argv, "Usage: -poly1 -poly2");
+  auto g_glog_guard = CreateGlogGuard("polyover_vk");
 
-  gflags::SetUsageMessage("Usage: -poly1 -poly2");
-  if (argc == 1) {
-    gflags::ShowUsageWithFlags(argv[0]);
-    exit(1);
-  }
-  gflags::ParseCommandLineFlags(&argc, &argv, true);
-  gflags::ShutDownCommandLineFlags();
-
-  google::InitGoogleLogging(argv[0]);
-  google::InstallFailureSignalHandler();
-
-  rayjoin::OverlayConfig config;
-  std::string exec_path = argv[0];
-
-  config.map1_path = FLAGS_poly1;
-  config.map2_path = FLAGS_poly2;
-  config.output_path = FLAGS_output;
-  config.serialize_prefix = FLAGS_serialize;
-  config.grid_size = FLAGS_grid_size;
-  config.xsect_factor = FLAGS_xsect_factor;
-  config.mode = FLAGS_mode;
-  config.exec_root = exec_path.substr(0, exec_path.find_last_of("/"));
-  config.check = FLAGS_check;
-  config.fau = FLAGS_fau;
-  config.profile = FLAGS_profile;
-  config.win = FLAGS_win;
-  config.ag = FLAGS_ag;
-  config.ag_iter = FLAGS_ag_iter;
-  config.enlarge = FLAGS_enlarge;
-
-  // dump
-  config.dump_results = FLAGS_dump_results;
-  config.dump_dir = FLAGS_dump_dir;
-
+  auto config = rayjoin::CreateOverlayConfig(argv[0]);
   rayjoin::RunOverlayNative(config);
-
-  google::ShutdownGoogleLogging();
 }
