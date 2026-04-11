f7ef872b7313271edc6d07304a83fe7875c35a93
victor-liu <jiaxinliu.victor@gmail.com>
2026-04-10 23:05:36 -0400

    add abstract testFixture

---
+++ test/vk_algo/test_vk_fixture.h
@@ -0,0 +1,51 @@
+#ifndef RAYJOIN_TEST_VK_FIXTURE_H
+#define RAYJOIN_TEST_VK_FIXTURE_H
+
+#include <gtest/gtest.h>
+#include <memory>
+#include <string>
+
+#include "util/guard_glog.h"
+#include "vk/core/vk_global_context.h"
+#include "vk/engine/vk_compute_context.h"
+
+namespace rayjoin::vk {
+
+class TestVkFixture : public ::testing::Test {
+ protected:
+  static void SetUpTestSuite() {
+    glog_guard_ = CreateGlogGuardAlsoToStderr("vk-algo-test");
+    vk_runtime_ = CreateVkGlobalRuntime();
+  }
+
+  static void TearDownTestSuite() {
+    vk_runtime_.reset();
+    glog_guard_.reset();
+  }
+
+  void SetUp() override {
+    auto info = ::testing::UnitTest::GetInstance()->current_test_info();
+    std::string test_suite = info->test_suite_name();
+    std::string test_name = info->name();
+    std::string prefix = glog_guard_->LogDir() + "/log-" + test_suite + "-" + test_name;
+
+    google::SetLogDestination(google::INFO, (prefix + ".INFO.").c_str());
+    google::SetLogDestination(google::WARNING, (prefix + ".WARNING.").c_str());
+    google::SetLogDestination(google::ERROR, (prefix + ".ERROR.").c_str());
+    google::SetLogDestination(google::FATAL, (prefix + ".FATAL.").c_str());
+
+    LOG(INFO) << "SetUp: configured log destinations for test " << test_suite << "." << test_name << " prefix=" << prefix;
+  }
+
+  void TearDown() override {
+    auto info = ::testing::UnitTest::GetInstance()->current_test_info();
+    LOG(INFO) << "TearDown: finished test " << info->test_suite_name() << "." << info->name();
+  }
+
+  inline static std::unique_ptr<GlogGuard> glog_guard_;
+  inline static std::unique_ptr<VkGlobalRuntime> vk_runtime_;
+};
+
+}  // namespace rayjoin::vk
+
+#endif  // RAYJOIN_TEST_VK_FIXTURE_H
